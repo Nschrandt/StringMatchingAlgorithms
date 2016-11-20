de060b98eec59d50c5e32a70eaad375a56ebbe9f
Kevin Omidvaran <omidkv@unm.edu>
2016-11-20 15:04:53 -0700

    Initial Commit

---
+++ StringMatching/rabinKarp.h
@@ -0,0 +1,14 @@
+//
+//  rabinKarp.h
+//  StringMatching
+//
+//  Created by Family on 11/16/16.
+//  Copyright © 2016 Family. All rights reserved.
+//
+
+#ifndef rabinKarp_h
+#define rabinKarp_h
+
+#include <stdio.h>
+
+#endif /* rabinKarp_h */
