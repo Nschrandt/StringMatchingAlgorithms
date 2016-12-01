8168bbad95d33228033d7b23b3edba1fa5ba8bc6
Kevin Omidvaran <omidkv@unm.edu>
2016-11-30 18:58:45 -0700

    Cleaned up naive.c and rabinKarp.c

--- StringMatching/naive.c
+++ StringMatching/naive.c
@@ -1,24 +1,18 @@
-//
-//  main.c
-//  StringMatching
-//
-//  Created by Family on 11/10/16.
-//  Copyright © 2016 Family. All rights reserved.
-//
+
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-char* intialize(FILE* in, int sizeOfPattern);
 char* intial(FILE* in);
+int readAndUpdate(const char* pattern, FILE* in);
+
 
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
 
-  char* infile;
-  char* outfile;
-  char* pattern;
+ const char* infile;
+ const char* pattern;
   int matches;
   
   FILE* in;
@@ -47,99 +41,48 @@ int main(int argc, const char * argv[]) {
   {
     printf("There were %d matches\n",matches);
   }
-  
-  
   return 0;
 }
-
-int readAndUpdate(char* pattern, FILE* in)
+/*
+ Read and update is a method that takes in a char * and file*
+ It first calls intial on the file* it then takes the returned char* and looks for 
+ matches of the pattern in it.
+ */
+int readAndUpdate(const char* pattern, FILE* in)
 {
-  char tem[2];
   char* tempString;
-  char charc;
   int sizeOfPattern;
   int i;
   int j;
   int matches = 0;
   int letterMatches =0;
-//  printf("%s \n ",pattern);
+  int sizeOfTemp;
   sizeOfPattern = strlen(pattern);
-//  printf("%d size is \n",sizeOfPattern);
   
   tempString = intial(in);
-  
-//  printf("hello\n");
+  sizeOfTemp = strlen(tempString);
 
- 
-
-  for(i=0;i<=strlen(tempString)-sizeOfPattern;i++)
+  for(i=0;i<=sizeOfTemp-sizeOfPattern;i++)
   {
     for(j=0;j<sizeOfPattern;j++)
     {
-      if(pattern[j] != tempString[i+j])
-      {
-        break;
-      }
-      else
-      {
-        letterMatches++;
-      }
+      if(pattern[j] != tempString[i+j]) break;
       
     }
-    if (letterMatches == sizeOfPattern)
+    if (j == sizeOfPattern)
     {
-//      printf("%c,%c",tempString[i],tempString[i+j]);
       matches++;
     }
     letterMatches =0;
-    
-  
-//    if(matching(pattern,strcpy(tempString+i,sizeOfPattern),sizeOfPattern))
-//    {
-//      printf("This two strings are matching %s %s \n",pattern,tempString);
-//      matches ++;
-//      
-//    }
   }
-
-//  printf("This sucks\n");
+  free(tempString);
   return matches;
-
 }
-//char* intialize(FILE* in, int sizeOfPattern)
-//{
-//  int i;
-//  char* intial;
-//  intial= malloc(sizeof(char*));
-//  char charc;
-//  char c[2];
-//  c[1]= '\0';
-//  for (i=0; i<sizeOfPattern-1; i++)
-//  {
-//    fscanf(in,"%c",&charc);
-//
-//    c[0] = charc;
-//    printf("%s\n",c);
-//
-//    strcat(intial,c);
-//  }
-////  printf("temp %s", intial);
-//  return intial;
-//}
-
-int matching(char* pattern, char* temp,int sizeOfPattern)
-{
-  int i;
-
-  for (i=0; i<sizeOfPattern-1; i++) {
-    if(pattern[i]!= temp[i])
-    {
-      return 0;
-    }
-  }
- 
-  return 1;
-}
+/*
+ Intial is a method that takes a file as a paramater.
+ With that file it checks the size and allocs memory for a char* of that size
+ It then reads the file into that char *
+ */
 char* intial(FILE* in)
 {
   long len;
@@ -149,7 +92,6 @@ char* intial(FILE* in)
   rewind(in);
   file = calloc(1,len+1);
   fread(file,len,1,in);
-//  printf("%s\n",file);
   return file;
 }
 
--- StringMatching/rabinKarp.c
+++ StringMatching/rabinKarp.c
@@ -1,28 +1,17 @@
-//
-//  rabinKarp.c
-//  StringMatching
-//
-//  Created by Family on 11/16/16.
-//  Copyright © 2016 Family. All rights reserved.
-//
-
-#include "rabinKarp.h"
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
 int prime;
-long patternHash(char* pattern, int alphabetSize, int length);
-long update(char letter, char minus, int alphabet, long hash, long h);
 char* intial(FILE* in);
+int reader(const char* pattern, FILE* in, int alphabet, long h);
 
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
+  
   long h;
-  char* infile;
-  char* outfile;
-  char* pattern;
+  const char* infile;
+  const char* pattern;
   int patternlength;
   int i;
   int alphabet;
@@ -50,7 +39,6 @@ int main(int argc, const char * argv[]) {
   {
     h = (h * alphabet)%prime;
   }
-//  h=(int)pow(alphabet, patternlength-1)%prime;
 
  matches = reader(pattern,in,alphabet,h);
   if(matches == 0)
@@ -65,138 +53,11 @@ int main(int argc, const char * argv[]) {
   
   return 0;
 }
-char* intialize(FILE* in, int sizeOfPattern)
-{
-  int i;
-  char* intial;
-  intial=(char*)malloc(sizeof(10200));
-  char charc;
-  char c[2];
-  c[1]='\0';
-  for (i=0; i<sizeOfPattern; i++)
-  {
-    fscanf(in,"%c",&charc);
-    c[0] = charc;
-    strcat(intial,c);
-  }
-  return intial;
-}
-
-int readAndUpdate(char* pattern, FILE* in, int alpha, long h)
-{
-  char tem[2];
-  char* tempString;
-  char* copy;
-  char charc;
-  int sizeOfPattern;
-  long hashPattern;
-  long hashTemp;
-  int alphabet;
-  int numberOfMatches = 0;
-  int passes = 0;
-  int i;
-  int j;
-  int k;
-  int letterMatches=0;
-  
-  tempString = malloc(sizeof(char*));
-  alphabet = alpha;
-  sizeOfPattern = strlen(pattern);
-  
- tempString = intial(in);
-  
- 
-  hashPattern = patternHash(pattern, alphabet,sizeOfPattern);
-  hashTemp = patternHash(tempString, alphabet,sizeOfPattern);
-  
-  if (hashPattern == hashTemp)
-  {
-//    printf("hashPattern %lu hashTemp %lu \n",hashPattern,hashTemp);
-    return 1;
-  }
-////  printf("hashPattern %lu hashTemp %lu \n",hashPattern,hashTemp);
-//  while((fscanf(in,"%c",&charc))!= EOF)
-//  {
-//    tem[0] = charc;
-////    strcat(tempString,tem);
-//    hashTemp = update(tem[0],tempString[passes%sizeOfPattern], alphabet, hashTemp,h);
-//    tempString[passes%sizeOfPattern] = tem[0];
-////    tempString = tempString+1;
-//    
-////    printf("hashPattern %s %lu hashTemp %s %lu \n",pattern,hashPattern,tempString,hashTemp);
-//    if(hashTemp == hashPattern)
-//    {
-////      printf("Match\n");
-//      printf("hashPattern %s %ld hashTemp %s %ld, index of first letter %d\n",pattern,hashPattern,tempString,hashTemp,passes%sizeOfPattern);
-//      numberOfMatches++;
-//
-//    }
-  for(i=0;i<strlen(tempString)-sizeOfPattern;i++)
-  {
-    if(hashPattern==hashTemp)
-    {
-      
-      for(j = 0;j<sizeOfPattern;j++)
-      {
-//        printf("Pattern[j+i] %c", tempString[j+i]);
-        if(pattern[j] != tempString[j+i])
-        {
-          break;
-        }
-        else
-        {
-          letterMatches++;
-        }
-      }
-      if(letterMatches == sizeOfPattern)
-      {
-        numberOfMatches++;
-      }
-      letterMatches = 0;
-    }
-   hashTemp = update(tempString[i+sizeOfPattern], tempString[i], 255, hashTemp, h);
-//    passes++;
-//          printf("hashPattern %s %ld hashTemp %s %ld\n",pattern,hashPattern,tempString,hashTemp);
-
-  }
-
-
-  
-//  tempString = tempString -3;
-  free(tempString);
-  return numberOfMatches;
-  
-}
-long patternHash(char* pattern, int alphabetSize, int length)
-{
-  int i;
-  long patternHash;
-  patternHash = 0;
-//  length = strlen(pattern);
-  for(i = 0; i<length; i++)
-  {
-    patternHash = ((alphabetSize*patternHash)+pattern[i])%prime;
-//    printf("Pattern Hash %lu\n", patternHash);
-  }
-  return patternHash;
-}
-long update(char letter, char minus, int alphabet, long hash, long h)
-{
-  long temp;
-  temp = ((minus)*h)%prime;
-//  printf("hash %lu", hash);
-//  printf("Temp 1 %lu\n",temp);
-  temp = (hash - temp)%prime;
-//  printf("Temp2 %d\n",temp);
-//  printf("Alpabet %d, Letter %c, minus %c, h %lu\n",alphabet,letter,minus,h);
-  
-  hash = (alphabet*(temp)+letter)%prime;
-  if(hash<0)
-  {
-    hash = hash+prime;
-  }
-  return hash;
-}
+/*
+ Intial is a method that takes a file as a paramater.
+ With that file it checks the size and allocs memory for a char* of that size
+ It then reads the file into that char *
+ */
 char* intial(FILE* in)
 {
   long len;
@@ -206,10 +67,15 @@ char* intial(FILE* in)
   rewind(in);
   file = calloc(1,len+1);
   fread(file,len,1,in);
-  //  printf("%s\n",file);
   return file;
 }
-int reader(char* pattern, FILE* in, int alphabet, long h)
+/*
+ reader is the method where all the work happens. Reader takes in a pattern
+ an in file and a long h. Pattern calls intial on the in file, it then builds the intial 
+ hash for pattern and tempString. It then tries to match. Will there are still possible
+ matches it updates the tempHash.
+ */
+int reader(const char* pattern, FILE* in, int alphabet, long h)
 {
   int matches;
   char * tempString;
@@ -228,14 +94,12 @@ int reader(char* pattern, FILE* in, int alphabet, long h)
     patternHash =((alphabet * patternHash)+pattern[i]) %prime;
     tempHash = ((alphabet * tempHash)+tempString[i]) %prime;
   }
-//  printf("Pattern Hash is %ld\n", patternHash);
-//  printf("Temp Hash is %ld\n", tempHash);
+
   for(j = 0; j< tempLength; j++)
   {
     
     if(patternHash == tempHash)
     {
-//      printf("One match was found");
       
       for(i = 0; i<patternLength;i++)
       {
@@ -248,19 +112,13 @@ int reader(char* pattern, FILE* in, int alphabet, long h)
       }
     
     }
-//    if(tempString[j-1] == 'c')
-//    {
-//      printf("hash is %ld", tempHash);
-//    }
     if(i<tempLength-patternLength)
     {
     tempHash = (alphabet*((tempHash - tempString[j]*h)%prime)+tempString[j+patternLength])%prime;
     if(tempHash<0)
     {
-//     printf("Hash is less than zero\n");
       tempHash = tempHash+prime;
     }
-//    printf("Hash is %ld",tempHash);
     }
   }
   
